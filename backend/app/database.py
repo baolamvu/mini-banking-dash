94ea78aa7b355607de49eb44478b9eff308bd052
lamvb <baolamvu98@gmail.com>
2026-07-05 22:45:09 +0700

    fix bug

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,7 @@ DATABASE_URL = os.getenv(
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
